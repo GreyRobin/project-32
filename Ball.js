977cff6ebf6335939aa57a23fc3c81a4df87d520
GreyRobin <86514645+GreyRobin@users.noreply.github.com>
2021-09-16 17:25:17 +1000

    Add files via upload

--- Ball.js
+++ Ball.js
@@ -1,7 +1,7 @@
-class Ball{
-    constructor(x,y,w,h){
-           let options = {
-      restitution: 0.08
+class Ball {
+  constructor(x, y, w, h) {
+    let options = {
+      restitution: 0.8
     };
 
     this.body = Bodies.rectangle(x, y, w, h, options);
@@ -9,16 +9,16 @@ class Ball{
     this.h = h;
 
     World.add(world, this.body);
-    }
-    show() {
-      var pos = this.body.position;
-      var angle = this.body.angle
-      push();
-      translate(pos.x, pos.y);
-      rotate(angle);
-      rectMode(CENTER);
-      fill("#8d6e63");
-      rect(0, 0, this.w, this.h);
-      pop();
-    }
+  }
+
+  show() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    ellipseMode(CENTER);
+    ellipse(0, 0, this.w, this.h);
+    pop();
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,25 @@
-var ball, blower, blowermouth
-var button
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ball;
+var blower;
+var blowerMouth;
+var button;
 
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  var canvas = createCanvas(500, 500);
+
+  engine = Engine.create();
+  world = engine.world;
+
   ball = new Ball(width / 2 + 80, height / 2 - 80, 80, 80);
   blower = new Blower(width / 2 - 50, height / 2 + 50, 150, 20);
   blowermouth = new Blowermouth(width / 2 + 70, height / 2 + 20, 100, 90);
@@ -14,14 +30,14 @@ function setup() {
 }
 
 function draw() {
-  background(255,255,255); 
+  background(59);
   Engine.update(engine);
- 
-  ball.show()
-  blower.show()
-  blowermouth.show()
-  
+
+  blower.show();
+  ball.show();
+  blowermouth.show();
 }
-function blow(){
-  Matter.Body.applyForce(ball.body,{x:0,y:0},{x:0,y:0.05})
+
+function blow() {
+  Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0, y: 0.05 });
 }
